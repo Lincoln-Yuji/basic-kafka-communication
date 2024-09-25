c8384437f52e12a20b46fd443590cd4639872516
Lincoln Yuji de Oliveira <lincolnyuji@hotmail.com>
2024-09-25 12:23:29 -0300

    Create the processor service and configure IN and OUT channels

---
+++ kafka-quickstart-processor/src/main/java/org/acme/kafka/processor/QuotesProcessor.java
@@ -0,0 +1,24 @@
+package org.acme.kafka.processor;
+
+import java.util.Random;
+
+import org.acme.kafka.model.Quote;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
+
+import io.smallrye.common.annotation.Blocking;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class QuotesProcessor {
+    
+    private Random rand = new Random();
+
+    @Incoming("requests")
+    @Outgoing("quotes")
+    @Blocking
+    public Quote process(String quoteRequest) throws InterruptedException {
+        Thread.sleep(200);
+        return new Quote(quoteRequest, rand.nextInt(100));
+    }
+}
